b067b72e657cf9eea612f6cf2735dea97f96e167
swanand09 <admin@DEV-REDDY1.mediacall.local>
2014-09-08 15:13:19 +0400

    1st commit

---
+++ app/code/local/Magentotutorial/Helloworld/controllers/IndexController.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ class Magentotutorial_Helloworld_IndexController extends Mage_Core_Controller_Front_Action {        
+
+    public function indexAction() {        
+        $this->loadLayout();
+        $this->renderLayout();
+
+    }
+    
+    public function goodbyeAction() {
+        echo 'Goodbye World!';
+    } 
+    
+    public function paramsAction() {
+        echo '<dl>';            
+        foreach($this->getRequest()->getParams() as $key=>$value) {
+            echo '<dt><strong>Param: </strong>'.$key.'</dt>';
+            echo '<dt><strong>Value: </strong>'.$value.'</dt>';
+        }
+        echo '</dl>';
+    } 
+} 
+?>
---
+++ app/code/local/Magentotutorial/Helloworld/controllers/MessagesController.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ class Magentotutorial_Helloworld_MessagesController extends Mage_Core_Controller_Front_Action {        
+     public function goodbyeAction()         
+    {
+       echo 'Another Goodbye';
+    } 
+ }
+?>
